170d82eef1299c1141f6cad2a4db6d98ec118f74
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-11-04 16:22:02 +0530

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_101926) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_103921) do
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -37,8 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_101926) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "friendships", "users"
